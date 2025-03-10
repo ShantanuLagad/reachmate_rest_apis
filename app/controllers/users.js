044a13a83838ae9a97c1db1ba43d880d55208b37
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-03-10 10:41:09 +0530

    done

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5562,12 +5562,12 @@ exports.checkIsTrialSubscriptionExisted = async (req, res) => {
   try {
     const userId = req.user._id;
     console.log("User ID : ", userId);
-    const trialSubscription = await Trial.findOne({ user_id: userId, status: "active" });
+    const trialSubscription = await Trial.findOne({ user_id: userId});
     console.log("trialSubscription : ", trialSubscription)
     if (trialSubscription) {
       return res.json({ message: "Trial Subscription Existed", data: trialSubscription, is_subscriber: true, code: 200 });
     }
-    const activeSubscription = await Subscription.findOne({ user_id: userId, status: "active" })
+    const activeSubscription = await Subscription.findOne({ user_id: userId})
     console.log("activeSubscription : ", activeSubscription)
     if (activeSubscription) {
       return res.json({ message: "Active Subscription Existed", data: activeSubscription, is_subscriber: true, code: 200 })
